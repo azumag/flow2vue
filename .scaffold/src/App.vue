9b005f477117ca4cf16d7dee2608bd144804c184
azumag <tsubasa.azumagakito@gmail.com>
2018-06-13 20:31:44 +0000

    replace app-title

--- .scaffold/src/App.vue
+++ .scaffold/src/App.vue
@@ -25,7 +25,7 @@
       </v-navigation-drawer>
       <v-toolbar color="indigo" dark fixed app>
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title >UGX Amazon FBA 相乗り配送サービス</v-toolbar-title>
+        <v-toolbar-title >APP_TITLE</v-toolbar-title>
       </v-toolbar>
       <v-content>
         <v-snackbar
