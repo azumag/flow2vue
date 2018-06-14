1dc998747a9ffdf3fc7f1917fbf70a9c9d28f51d
azumag <tsubasa.azumagakito@gmail.com>
2018-06-14 07:21:01 +0000

    parser for transitions

--- gen.rb
+++ gen.rb
@@ -3,7 +3,10 @@
 require 'fileutils'
 
 flow = File.read('flow.txt')
-puts flow
+flow << "\n" # insert \n for easy parsing
+#puts flow
+#
+APP_TITLE = 'VUE_GEN_TITLE'
 
 page_scans = flow.scan(/\[(.*)\]/).flatten
 
@@ -24,6 +27,19 @@ class Generator
     FileUtils.cp_r('.scaffold', 'dst')
   end
 
+  def rewrite_app_title
+    app_file = 'dst/src/App.vue'
+    FileUtils.cp('.scaffold/src/App.vue', app_file)
+    app = File.read(app_file)
+    app.gsub!('APP_TITLE', APP_TITLE)
+    File.write(app_file, app)
+  end
+
+  def copy_routes
+    routes_file = "dst/src/routes.js"
+    FileUtils.cp('.scaffold/src/routes.js', routes_file)
+  end
+
   def generate_route(page)
     routes_file = "dst/src/routes.js"
     routes = File.read(routes_file)
@@ -32,7 +48,7 @@ class Generator
 
     components = "\n},\n{\n path: '/#{page.name.downcase}', component: #{page.name} },\n]"
 
-    routes.gsub!(/},\n]/, components)
+    routes.gsub!(/\},\n\]/, components)
 
     File.write(routes_file, import + routes)
   end
@@ -49,7 +65,7 @@ class Generator
     trs = page.transitions.map do |trn|
       next unless trn
       # TODO: structurize
-      "<router-link to='/#{trn[1].downcase.strip}'>#{trn.first}</router-link>"
+      "<router-link to='/#{trn[2].downcase.strip}'>#{trn.first}</router-link>/"
     end
     pagesrc.gsub!('===LINK===', trs.join)
 
@@ -60,14 +76,39 @@ end
 
 gen = Generator.new
 gen.generate_scaffold unless File.exist?('dst')
+#gen.generate_scaffold 
+gen.rewrite_app_title
+gen.copy_routes
 
 pages = []
 page_scans.each do |page_scan|
   page_body = flow.scan(/^\[#{page_scan}\]$(.*?)(^\[|^\n)/m)
+  #pp flow
+  #pp flow.scan(/\[#{page_scan}\]\n(.*?)^\n$/m)
   transitions = []
-  transitions << page_body.to_s.scan(/---(.*?)==>(.*?)\\n/).first
-  page_body.to_s.scan(/==>.*?\\n(.*?)\\n==>(.*?)\\n/).each do |tr|
-    transitions << tr
+ 
+  ## parse transitions
+  tr_state = nil
+  trs = []
+  page_body.join.each_line do |tr|
+    unless tr.scan('---').compact.empty?
+      tr_state = :head
+      next
+    end
+    case tr_state
+    when :head
+      trs = [tr]
+      tr_state = :arrow
+    when :arrow
+      trs << tr.scan(/=({.*?})?=>(.*)/).flatten
+      transitions << trs.flatten
+      tr_state = :head
+    end
+  end
+
+  forms = []
+  page_body.join.each_line do |line|
+
   end
 
   page = Page.new(
@@ -77,11 +118,8 @@ page_scans.each do |page_scan|
     page_body.to_s.scan(/DisplayName\(.*?\)((.|\r|\n)*?)---/m),
     transitions
   )
-  pp page
-  #pp page_body.to_s.scan(/(---|=.*?\\n)((.|\r|\n)*?)=(.*?)/)
-  #gen.generate_route(page)
+  gen.generate_route(page)
   gen.generate_with(page) 
 end
-
 
 
